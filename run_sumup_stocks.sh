73c03392f746f15175e2cf98f2bcdbf22a7e661e
SKOHscripts <corentin.michel@mailo.com>
2026-04-04 15:14:43 +0200

    update

--- run_sumup_stocks.sh
+++ run_sumup_stocks.sh
@@ -4,5 +4,4 @@ set -e
 cd /home/skoh/SynologyDrive/Documents/Scripts/sumup
 /usr/bin/git pull
 
-/usr/bin/python3 /home/skoh/SynologyDrive/Documents/Scripts/sumup/sumup_stocks.py \
-  >> /home/skoh/SynologyDrive/Documents/Scripts/sumup/sumup_stocks.log 2>&1
+/usr/bin/python3 /home/skoh/SynologyDrive/Documents/Scripts/sumup/sumup_stocks.py  >> /home/skoh/SynologyDrive/Documents/Scripts/sumup/sumup_stocks.log 2>&1
