bab0c4a7eda1be4e68ee18675fb73d6cd93b7e32
SKOHscripts <corentin.michel@mailo.com>
2026-07-04 00:38:51 +0200

    auto: maj des stocks suite à l'exécution précédente

--- run.sh
+++ run.sh
@@ -78,6 +78,7 @@ case "$MODULE" in
 
         # Mise à jour du modèle
         "$PYTHON" -m stocks.ml.train --tune
+        "$PYTHON" -m stocks.ml.train
 
         # Rapport hebdomadaire + recalage local (décompte des ventes)
         "$PYTHON" -m stocks.sumup_stocks --ml "$@"
